82f01ddd2b6612c8dea09c5909a603a9dc3a0cc3
Matt Engerer <matt.engerer@aol.com>
2022-09-03 19:21:46 -0400

    first commit- roman to int problem w/ comments

---
+++ leetcode-practice-1.js
@@ -0,0 +1,54 @@
+// Roman Integer problem 
+
+// first, create object in order to be able to look up the meaning of each of these.
+const romanHash = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+
+  const s = "MCMLXXXIX";
+
+ function romanToInt(s) {
+  let accumulator = 0;
+      // iterate through the string.
+  for (let i = 0; i < s.length; i++) {
+
+        // essentially, this checks for all the cases in which subtraction takes place - the cases where a non-intuitive combination of RN is used. checks the current index of i AND the next index, and whether they form any of the 'IV', 'IX' combinations. if so, those specific numbers are added to the accumulator, and i is incremented.
+
+        // in every other case where these combinations are NOT found, the 'usual' numerical equivalent of each RN is added to the accumulator (the RN found in the current index of s -look up this key in the romanHash object and get the correct value); increment.
+
+        // in the end, return the full integer found in the accumulator.
+    if (s[i] === "I" && s[i + 1] === "V") {
+      accumulator += 4;
+      i++;
+    } else if (s[i] === "I" && s[i + 1] === "X") {
+      accumulator += 9;
+      i++;
+    } else if (s[i] === "X" && s[i + 1] === "L") {
+      accumulator += 40;
+      i++;
+    } else if (s[i] === "X" && s[i + 1] === "C") {
+      accumulator += 90;
+      i++;
+    } else if (s[i] === "C" && s[i + 1] === "D") {
+      accumulator += 400;
+      i++;
+    } else if (s[i] === "C" && s[i + 1] === "M") {
+      accumulator += 900;
+      i++;
+    } else {
+      accumulator += romanHash[s[i]];
+    }
+  }
+  console.log(accumulator)
+  return accumulator;
+}
+
+
+romanToInt(s)
