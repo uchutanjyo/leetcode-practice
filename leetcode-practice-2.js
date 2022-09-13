323b253d936457cc6d11b3810c0e2d1c5810cf68
Matt Engerer <matt.engerer@aol.com>
2022-09-13 01:22:07 -0400

    increment by one problem - not yet complete

--- leetcode-practice-2.js
+++ leetcode-practice-2.js
@@ -5,25 +5,22 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-var plusOne = function (digits) {
+ var plusOne = function (digits) {
     for (let i = 0; i <= digits.length; i++) {
         if (i === digits.length - 1) {
             digits[i] += 1
-            if (digits[i] === 10) {
-                if (digits.length === 1) {
-                    digits = [1, 0]
-                } else if (digits.length === 2) {
-                    digits = [1, 0, 0]
-                } else if (digits.length === 3) {
-                    digits = [1, 0, 0, 0]
+        }
+        if (digits[i] === 10) {
+            if (digits.length === 1) {
+                return digits = [1, 0]
+            } else if (digits === [9,9]) {
+                return digits = [1, 0, 0]
+            } else if (digits === [9,9,9]) {
 
-                } else {
-                    (digits[i - 1]) += 1;
-                    digits[i] = 0
-                }
-                return digits
-            }
-            if (digits.length === 4 && digits[i] === 10) {
+
+                return digits = [1, 0, 0, 0]
+
+            } else if (digits.length === 4 && digits[i] === 10) {
                 if (digits[i] === 10 && digits[i - 2] === 9 && !digits[i - 3] === 9) {
                     (digits[i - 1]) = 1;
                     digits[i] = 0
@@ -34,12 +31,20 @@ var plusOne = function (digits) {
                     digits[i - 1] = 0
                     digits[i] = 0
                 }
+            } else {
+
+                (digits[i - 1]) += 1;
+                digits[i] = 0
             }
+            console.log(digits)
+
+
         }
     }
+   
     return digits
 
 };
 
 
-plusOne([9])
+plusOne([9, 9])
