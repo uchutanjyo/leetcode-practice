c3c5d655df6f5a483b4a7dfce83c460fb8bc28ab
Matt Engerer <matt.engerer@aol.com>
2022-09-21 17:51:19 -0400

    truncate sentence complete

--- leetcode-practice-2.js
+++ leetcode-practice-2.js
@@ -202,3 +202,25 @@ var balancedStringSplit = function (s) {
 };
 
 balancedStringSplit('RLRLRRLL')
+
+
+// truncate sentence problem
+
+var truncateSentence = function (s, k) {
+    let counter = 0;
+    let truncatedSentence
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == " ") {
+            counter++
+        }
+        if (counter == k) {
+            truncatedSentence = s.slice(0, i)
+            return truncatedSentence
+        }
+         else if (counter == k - 1 && !s.slice(i).includes(" ")) {
+            return s
+        }
+    }
+};
+
+truncateSentence('hi there im matt ok ok ok', 7)
