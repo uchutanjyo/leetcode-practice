2345534c25b3810d7661cacde6b20f46ec710ef2
Matt Engerer <matt.engerer@aol.com>
2022-09-26 13:42:30 -0400

    substring pattern problem - not complete

---
+++ leetcode-practice-3.js
@@ -0,0 +1,32 @@
+var repeatedSubstringPattern = function(s) {
+    let substrings = []
+    for (let i=0;i<s.length;i++) {
+        if (s.length % 2 !== 0) {
+            console.log('nope')
+            return false
+        }
+        substrings.push(s[i])
+        if (s[i - 1] && s[i-1] !== s[i]) {
+             substrings[i] += s[i+1]
+            console.log(substrings)
+            // s.includes(substrings[i]) ? true : false
+        }
+        if (substrings[i] += substrings[i] === s) {
+            console.log(substrings)
+            return true
+        } else if (substrings[i] += substrings[i] === s) {
+
+        }
+
+        else {
+            console.log('noo')
+            return false
+        }
+    }
+
+    
+};
+
+repeatedSubstringPattern('abcabcabcabc')
+
+        // if (s.includes(s[i])) {
